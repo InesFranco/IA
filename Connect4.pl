fad1b0209e0cc2e49e4dea16732de57684dafb5e
Ines Franco <ines_fra@hotmail.com>
2022-03-25 12:48:19 +0000

    progrress

--- Connect4.pl
+++ Connect4.pl
@@ -1,27 +1,27 @@
 /*6X7 board*/
-program() :- Board = 
-        [
-            [' ',' ',' ',' ',' ',' ',' '], 
-            [' ',' ',' ',' ',' ',' ',' '], 
-            [' ',' ',' ',' ',' ',' ',' '], 
-            [' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' '], 
-            [' ',' ',' ',' ',' ',' ',' ']
-        ],
-    draw_banner(), 
-    draw_board(Board),
-    read(Column),
-    write(Column),
-    /*place_piece(Column, Board),*/
-    program().
+program(Board) :- draw_banner(), 
+                draw_board(Board),
+                take_input(Column),
+                place_piece(Column, 'X', Board, NewBoard),
+                program(NewBoard).
     
-place_piece(C, B) :- print("hello"). 
+
+
+take_input(I) :- writeln("Write Column followed by a '.'"),
+                read(I),
+                writeln("").
+
+place_piece(C, P, B, NB) :- place_piece1(C, 1, P, B, NB).
+place_piece1(C, C, P, [[' '|T]|R] , [[P|T]|R]).
+place_piece1(C, N, P, [[H|T]|R], [[H|T1]|R]) :- N1 is N + 1
+                                    ,place_piece1(C, N1, P, [T|R], [T1|R]).
 
 draw_board([]).
 draw_board(B) :-    writeln("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |"),
                     writeln(" ___________________________"),
                     draw_board1(B).
 
+draw_board1([]).
 draw_board1([H|T]) :- draw_line(H), 
                     draw_board1(T).
 
@@ -46,5 +46,13 @@ draw_banner() :- writeln("       ___________"),
                  writeln("").
                  
 
-
+/*Board = 
+        program([
+            [' ',' ',' ',' ',' ',' ',' '], 
+            [' ',' ',' ',' ',' ',' ',' '], 
+            [' ',' ',' ',' ',' ',' ',' '], 
+            [' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ',' ',' '], 
+            [' ',' ',' ',' ',' ',' ',' ']
+        ]).*/
 
