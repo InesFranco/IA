cb68df20f5b75ec006ef24fa50c8153c0919de47
A47248 <A47248@alunos.isel.pt>
2022-04-19 14:21:55 +0100

    play without brain

--- Diogo/trab1v5-menosEficiente.pl
+++ Diogo/trab1v5-menosEficiente.pl
@@ -7,7 +7,7 @@
 */
 
 % apresenta o tabuleiro e o indice das colunas
-draw(B):-header(B,Division),invert(B,I),showLines(I,Division).
+draw(B):-header(B,Division),reverse(B,I),showLines(I,Division).
 
 % cria a os indices e devolve a linha horizintal(de separacao)
 header([L|_],Division):-length(L,X),columnum(1,X),division(0,X,'+',Division),writeln(Division).
@@ -39,8 +39,14 @@ showLine([E|R]):-writef('| %w ',[E]),showLine(R).
 % padrao do 4 em linha
 game:-gamedim(6,7,4).
 
+% padrao do jogo do galo
+gameAI:-gamedimAI(3,3,3).
+
 % predicado que permite configurar o jogo(dimensao do tabuleiro e de linha)
 gamedim(L,C,N):-build_board(L,C,B),draw(B),calc_tiles(B,X),asserta(win_dim(N)),asserta(line_dim(L)),game(B,X/'X'/'-').
+
+% predicado que permite configurar o jogo(dimensao do tabuleiro e de linha) com AI
+gamedimAI(L,C,N):-build_board(L,C,B),draw(B),calc_tiles(B,X),asserta(win_dim(N)),asserta(line_dim(L)),gameAI(B,X/'X'/'-').
 
 % contrutor do tabuleiro de jogo
 build_board(L,C,B):-make_line(1,C,[' '],Line),make_board(1,L,[Line],B).
@@ -72,7 +78,7 @@ game(B,Estado):-play(B,NB,Estado,NEstado),draw(NB),game(NB,NEstado).
 
 
 % recebe uma board e retorna o indice das posicoes possiveis(comeca em 1)
-possible_play(Board,PosList):-invert(Board,[L|_]),empty_pos(L,1,[],PosList).
+possible_play(Board,PosList):-reverse(Board,[L|_]),empty_pos(L,1,[],PosList).
 empty_pos([],_,PosList,PosList).
 empty_pos([' '|R],N,Aux,PosList):-K is N + 1,!,empty_pos(R,K,[N|Aux],PosList).   
 empty_pos([_|R],N,Aux,PosList):-K is N + 1,!,empty_pos(R,K,Aux,PosList).
@@ -85,7 +91,7 @@ input_play(Board,Pos):-writeln('jogada invalida'),input_play(Board,Pos).
 play(B,NB,Estado,NEstado):-input_play(B,Pos),place(B,Pos,NB,Estado,EstAux,1,Cord),checkWin(NB,Estado,EstAux,NEstado,Cord).
 
 % coloca a peca do jogador no tabuleiro
-place([L|R],Pos,[NL|R],X/P/_,NEstado,N,(N,Pos)):-placeAux(L,Pos,P,NL,1),novoEstado(X/P/_,NEstado).
+place([L|R],Pos,[NL|R],X/P/_,NEstado,N,(N,Pos)):-placeAux(L,Pos,P,NL,1),novoEstado(X/P/_,NEstado),!.
 place([L|R],Pos,[L|NR],Estado,NEstado,N,Cord):-K is N + 1,place(R,Pos,NR,Estado,NEstado,K,Cord).
 placeAux([X|R],Pos,P,[P|R],Pos):-!,X=' '.
 placeAux([X|R],Pos,P,[X|NR],N):-K is N+1,placeAux(R,Pos,P,NR,K).
@@ -102,7 +108,7 @@ player('X','O').
 checkWin(NB,_/P/_,_,_/P/'W',(L,_)):-horizontal(NB,P,1,L).
 checkWin(NB,_/P/_,_,_/P/'W',(_,C)):-vertical(NB,P,1,C).
 checkWin(NB,_/P/_,_,_/P/'W',Cord):-diagonal(NB,P,Cord).
-checkWin(NB,_/P/_,_,_/P/'W',(L,C)):-length(NB,X),La is X - L,invert(NB,I),diagonal(I,P,(La,C)).
+checkWin(NB,_/P/_,_,_/P/'W',(L,C)):-length(NB,X),La is X - L + 1,reverse(NB,I),diagonal(I,P,(La,C)).
 checkWin(_,_,NEstado,NEstado,_).
 
 % verifica a linha
@@ -120,7 +126,7 @@ vertAux2([],[]).
 vertAux2([[_|L]|R1],[L|R2]):-vertAux2(R1,R2).
 
 %verifica a diagonal
-diagonal(B,P,(L,C)):-X is L - C + 1, X > 0,diagonalAux(B,1,(X,1),Line),line(Line,P).
+diagonal(B,P,(L,C)):-X is L - C + 1, X > 0,diagonalAux(B,1,(X,1),Line),!,line(Line,P).
 diagonal(B,P,(L,C)):-X is C - L + 1, X > 1,diagonalAux(B,1,(1,X),Line),line(Line,P).
 
 diagonalAux([H|T],L,(L,C),D):-length(H,Len),getDiagonal([H|T],C,D,Len).
@@ -134,12 +140,6 @@ getDiagonal([L|R],C,[E|D],Len):-getCol(L,1,C,E),Ca is C+1,getDiagonal(R,Ca,D,Len
 % obtem o valor da coluna
 getCol([E|_],C,C,E).
 getCol([_|R],N,C,E):-K is N + 1,getCol(R,K,C,E).
-
-% inverte a lista
-invert(L,I):-invAux(L,[],I).
-
-invAux([],I,I).
-invAux([X|R],Aux,I):-invAux(R,[X|Aux],I).
 
 % verifica a linha(condicao de vitoria)
 line(L,P):-win_dim(X),lineX(L,P,0,X).
@@ -157,6 +157,15 @@ lineX([_|R],P,_,X):-lineX(R,P,0,X).
 		  Bot
 */
 
+% jogabilidade e apresentacao do jogo(jogar e mostrar tabuleiro/fim do jogo)
+gameAI(_,_/P/'W'):-writef('O player %w ganhou',[P]),retract(win_dim(_)).
+gameAI(_,0/_/'-'):-writeln('Empate'),retract(win_dim(_)).
+gameAI(B,X/'X'/_):-writeln('AI turn'),playAI(B,NB,X/'X'/'-',NEstado),draw(NB),gameAI(NB,NEstado).
+gameAI(B,Estado):-play(B,NB,Estado,NEstado),draw(NB),gameAI(NB,NEstado).
+
+
+% jogada da IA e atualizacao do estado
+playAI(B,NB,Estado,NEstado):-minimax(Estado-B-_,EstAux-NB-Cord,Val),checkWin(NB,Estado,EstAux,NEstado,Cord).
 
 % minimax( Pos, BestSucc, Val):
 %   Pos is a position, Val is its minimax value;
@@ -167,10 +176,12 @@ minimax( Pos, BestSucc, Val) :-
    ; % Or
    staticval( Pos, Val).         % Pos has no successors: evaluate statically
    
-moves(Estado-B-(_),PosList):-possible_play(B,PP),findall(NEstado-NB-(Cord),playAI(B,Estado,NB,NEstado,PP,Cord),PosList).
+% gera a lista com todas as jogadas possive
+moves(Estado-B-_,PosList):-possible_play(B,PP),findall(NEstado-NB-Cord,move(B,Estado,NB,NEstado,PP,Cord),PosList),PosList\=[].
 
-playAI(B,Estado,NB,NEstado,PP,Cord):-member(PP,Pos),place(B,Pos,NB,Estado,NEstado,1,Cord),print([Cord]).
-   
+move(B,Estado,NB,NEstado,PP,Cord):-member(Pos,PP),place(B,Pos,NB,Estado,NEstado,1,Cord).
+     
+ 
    
 best( [Pos], Pos, Val) :-
    minimax( Pos, _, Val), !.
@@ -192,5 +203,10 @@ betterof( Pos0, Val0, Pos1, Val1, Pos0, Val0) :- % Pos0 better than Pos1
 
 betterof( Pos0, Val0, Pos1, Val1, Pos1, Val1). % Otherwise Pos1 better than Pos0
 
-max_to_move('X').
-min_to_move('O').
+max_to_move(_/'X'/_-_-_).
+min_to_move(_/'O'/_-_-_).
+
+
+staticval(_/'X'/'W'-_-_, 1).
+staticval(_/'O'/'W'-_-_, -1).
+staticval(_,0).
